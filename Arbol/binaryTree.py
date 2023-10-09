97ab5f0e3cd0413ceab51ee4abd017eb263cca44
Mariano <sosamariano5@gmail.com>
2023-10-08 22:02:04 -0300

    Inicio de TP5 de arbol y renombre TP4 de lista

--- Arbol/binaryTree.py
+++ Arbol/binaryTree.py
@@ -1,9 +1,23 @@
+import sys #Import library 'sys'
+sys.path.append('/home/ms/Algoritmo_2023_MS/Cola') 
+from colaClass import Cola
+import linecache
+
+
+def get_value_from_file(file_name, index):
+    line = linecache.getline(file_name, index)
+    line_split = line.split(';')
+    line_split.pop()
+    return line_split
+
 class NodeTree():
 
-    def __init__(self, value):
+    def __init__(self, value, other_values=None):
         self.value = value
+        self.other_values = other_values
         self.left = None
         self.right = None
+        self.height = 0
 
 
 class BinaryTree:
@@ -11,18 +25,82 @@ class BinaryTree:
     def __init__(self):
         self.root = None
 
-    def insert_node(self, value):
+    def height(self, root):
+        if root is None:
+            return -1
+        else:
+            return root.height
 
-        def __insertar(root, value):
+    def update_height(self, root):
+        if root is not None:
+            left_height = self.height(root.left)
+            right_height = self.height(root.right)
+            root.height = (left_height if left_height > right_height else right_height) + 1
+
+    def simple_rotation(self, root, control):
+        if control:
+            aux = root.left
+            root.left = aux.right
+            aux.right = root
+        else:
+            aux = root.right
+            root.right = aux.left
+            aux.left = root
+        self.update_height(root)
+        self.update_height(aux)
+        root = aux
+        return root
+
+    def double_rotation(self, root, control):
+        if control:
+            root.left = self.simple_rotation(root.left, False)
+            root = self.simple_rotation(root, True)
+        else:
+            root.right = self.simple_rotation(root.right, True)
+            root = self.simple_rotation(root, False)
+        return root
+
+    def balancing(self, root):
+        if root is not None:
+            if self.height(root.left) - self.height(root.right) == 2:
+                if self.height(root.left.left) >= self.height(root.left.right):
+                    root = self.simple_rotation(root, True)
+                else:
+                    root = self.double_rotation(root, True)
+            elif self.height(root.right) - self.height(root.left) == 2:
+                if self.height(root.right.right) >= self.height(root.right.left):
+                    root = self.simple_rotation(root, False)
+                else:
+                    root = self.double_rotation(root, False)
+        return root
+
+    def insert_node(self, value, other_values=None):
+
+        def __insertar(root, value, other_values):
             if root is None:
-                return NodeTree(value)
+                return NodeTree(value, other_values)
             elif value < root.value:
-                root.left = __insertar(root.left, value)
+                root.left = __insertar(root.left, value, other_values)
             else:
-                root.right = __insertar(root.right, value)
+                root.right = __insertar(root.right, value, other_values)
+            root = self.balancing(root)
+            self.update_height(root)
             return root
 
-        self.root = __insertar(self.root, value)
+        self.root = __insertar(self.root, value, other_values)
+
+    def by_level(self):
+        if self.root is not None:
+            cola_tree = Cola()
+            cola_tree.arrive(self.root)
+            while cola_tree.size() > 0:
+                node = cola_tree.atention()
+                print(node.value)
+                # a = input()
+                if node.left is not None:
+                    cola_tree.arrive(node.left)
+                if node.right is not None:
+                    cola_tree.arrive(node.right)
 
     def inorden(self):
         def __inorden(root):
@@ -32,6 +110,55 @@ class BinaryTree:
                 __inorden(root.right)
 
         __inorden(self.root)
+
+    def inorden_file(self, file_name):
+        def __inorden_file(root, file_name):
+            if root is not None:
+                __inorden_file(root.left, file_name)
+                value = get_value_from_file(file_name, root.other_values)
+                print(root.value, value[0])
+                __inorden_file(root.right, file_name)
+
+        __inorden_file(self.root, file_name)
+
+    def inorden_file_lightsaber(self, file_name, lightsaber_color):
+        def __inorden_file_lightsaber(root, file_name, lightsaber_color):
+            if root is not None:
+                __inorden_file_lightsaber(root.left, file_name, lightsaber_color)
+                value = get_value_from_file(file_name, root.other_values)
+                if lightsaber_color in value[4].split('/'):
+                    print(root.value, value[4].split('/'))
+                __inorden_file_lightsaber(root.right, file_name, lightsaber_color)
+
+        __inorden_file_lightsaber(self.root, file_name, lightsaber_color)
+
+    def inorden_super_or_villano(self, is_hero):
+        def __inorden_s_v(root, is_hero):
+            if root is not None:
+                __inorden_s_v(root.left, is_hero)
+                if root.other_values is is_hero:
+                    print(root.value)
+                __inorden_s_v(root.right, is_hero)
+
+        __inorden_s_v(self.root, is_hero)
+
+    def inorden_start_with(self, cadena):
+        def __inorden_start_with(root, cadena):
+            if root is not None:
+                __inorden_start_with(root.left, cadena)
+                if root.other_values is True and root.value.upper().startswith(cadena):
+                    print(root.value)
+                __inorden_start_with(root.right, cadena)
+
+    def inorden_start_with_jedi(self, cadena):
+        def __inorden_start_with_jedi(root, cadena):
+            if root is not None:
+                __inorden_start_with_jedi(root.left, cadena)
+                if root.value.upper().startswith(cadena):
+                    print(root.value)
+                __inorden_start_with_jedi(root.right, cadena)
+
+        __inorden_start_with_jedi(self.root, cadena)
 
     def postorden(self):
         def __postorden(root):
@@ -45,11 +172,21 @@ class BinaryTree:
     def preorden(self):
         def __preorden(root):
             if root is not None:
-                print(root.value)
+                print(root.value, root.height)
                 __preorden(root.left)
                 __preorden(root.right)
 
         __preorden(self.root)
+
+    def search_by_coincidence(self, value):
+        def __search_by_coincidence(root, value):
+            if root is not None:
+                if root.value.startswith(value):
+                    print(root.value)
+                __search_by_coincidence(root.left, value)
+                __search_by_coincidence(root.right, value)
+
+        __search_by_coincidence(self.root, value)
 
     def search(self, key):
         def __search(root, key):
@@ -97,5 +234,26 @@ class BinaryTree:
             self.root, delete_value = __delete(self.root, key)
         return delete_value
 
+    def contar(self, value):
+        def __contar(root, value):
+            count = 0
+            if root is not None:
+                if root.value == value:
+                    count = 1
+                count += __contar(root.left, value)
+                count += __contar(root.right, value)
+            return count
 
+        return __contar(self.root, value)
+    
+    def contar_heroes(self):
+        def __contar_heroes(root):
+            count = 0
+            if root is not None:
+                if root.other_values is True:
+                    count = 1
+                count += __contar_heroes(root.left)
+                count += __contar_heroes(root.right)
+            return count
 
+        return __contar_heroes(self.root)
